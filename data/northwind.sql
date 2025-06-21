083844c31926b25d2daf1cd262fcf7ce67ac3b33
Sowmya Maddali <maddalisowmya82@gmail.com>
2025-06-20 20:05:03 -0400

    Organizing the files in appropriate folder

--- data/northwind.sql
+++ data/northwind.sql
@@ -2,18 +2,18 @@
 -- PostgreSQL database dump
 --
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
+-- SET statement_timeout = 0;
+-- SET lock_timeout = 0;
+-- SET client_encoding = 'UTF8';
+-- SET standard_conforming_strings = on;
+-- SET check_function_bodies = false;
+-- SET client_min_messages = warning;
 
 
 
-SET default_tablespace = '';
+-- SET default_tablespace = '';
 
-SET default_with_oids = false;
+-- SET default_with_oids = false;
 
 
 ---
@@ -21,20 +21,20 @@ SET default_with_oids = false;
 ---
 
 
-DROP TABLE IF EXISTS customer_customer_demo;
-DROP TABLE IF EXISTS customer_demographics;
-DROP TABLE IF EXISTS employee_territories;
-DROP TABLE IF EXISTS order_details;
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS shippers;
-DROP TABLE IF EXISTS suppliers;
-DROP TABLE IF EXISTS territories;
-DROP TABLE IF EXISTS us_states;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS region;
-DROP TABLE IF EXISTS employees;
+-- DROP TABLE IF EXISTS customer_customer_demo;
+-- DROP TABLE IF EXISTS customer_demographics;
+-- DROP TABLE IF EXISTS employee_territories;
+-- DROP TABLE IF EXISTS order_details;
+-- DROP TABLE IF EXISTS orders;
+-- DROP TABLE IF EXISTS customers;
+-- DROP TABLE IF EXISTS products;
+-- DROP TABLE IF EXISTS shippers;
+-- DROP TABLE IF EXISTS suppliers;
+-- DROP TABLE IF EXISTS territories;
+-- DROP TABLE IF EXISTS us_states;
+-- DROP TABLE IF EXISTS categories;
+-- DROP TABLE IF EXISTS region;
+-- DROP TABLE IF EXISTS employees;
 
 --
 -- Name: categories; Type: TABLE; Schema: public; Owner: -; Tablespace: 
